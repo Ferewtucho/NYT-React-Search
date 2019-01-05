41837c0075e067be72685999e31c7878773ce3cb
Ferew Tucho <ferewtucho@gmail.com>
2019-01-05 18:30:57 -0500

    change mongodb

--- server.js
+++ server.js
@@ -1,61 +1,57 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-var Article = require('./models/Article.js');
+var Article = require("./models/Article.js");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-var link = 'mongodb://heroku_f20841fp:ikh0mauhe69khuctk9v4lip93b@ds147864.mlab.com:47864/heroku_f20841fp';
-//Local link
-// var link = 'mongodb://localhost/nytreact';
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
-mongoose.connect(link);
 var db = mongoose.connection;
 
-db.on('error', function (err) {
-  console.log('Mongoose Error: ', err);
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
 });
 
-db.once('open', function () {
-  console.log('Mongoose connection successful.');
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
 });
 
-app.get('/', function(req, res){
-  res.sendFile('./public/index.html');
-})
-
-app.get('/api/saved', function(req, res) {
-
-  Article.find({})
-    .exec(function(err, doc){
-
-      if(err){
-        console.log(err);
-      }
-      else {
-        res.send(doc);
-      }
-    })
+app.get("/", function(req, res) {
+  res.sendFile("./public/index.html");
 });
 
-app.post('/api/saved', function(req, res){
+app.get("/api/saved", function(req, res) {
+  Article.find({}).exec(function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(doc);
+    }
+  });
+});
+
+app.post("/api/saved", function(req, res) {
   var newArticle = new Article(req.body);
 
   var title = req.body.title;
   var date = req.body.date;
   var url = req.body.url;
 
-  newArticle.save(function(err, doc){
-    if(err){
+  newArticle.save(function(err, doc) {
+    if (err) {
       console.log(err);
     } else {
       res.send(doc._id);
@@ -63,20 +59,19 @@ app.post('/api/saved', function(req, res){
   });
 });
 
-app.delete('/api/saved/', function(req, res){
+app.delete("/api/saved/", function(req, res) {
+  var url = req.param("url");
 
-  var url = req.param('url');
-
-  Article.find({"url": url}).remove().exec(function(err, data){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.send("Deleted");
-    }
-  });
+  Article.find({ url: url })
+    .remove()
+    .exec(function(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Deleted");
+      }
+    });
 });
-
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
